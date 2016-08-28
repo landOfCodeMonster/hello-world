13d6507b163a0be8c37edf8ad50d3acb5aa0d009
Landry <mamanlandry@gmail.com>
2016-08-28 10:05:17 -0700

    premier commit

---
+++ README.md
@@ -0,0 +1,4 @@
+E-juriste
+=========
+
+A Symfony project created on August 28, 2016, 9:53 am.
---
+++ app/config/deploy.rb
@@ -0,0 +1,40 @@
+set :application, "E-juriste"
+set :domain,      "127.0.0.1"
+set :user,		  "maman"
+set :use_sudo,	  false
+set :deploy_to,   "/home/maman/Documents/remote/E-juriste"    
+set :app_path,    "app"
+
+set :repository,  "maman@127.0.0.1:/Documents/E-juriste"
+#set :deploy_via,  :copy
+set :scm,         :git
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `subversion`, `mercurial`, `perforce`, or `none`
+
+set :model_manager, "doctrine"
+# Or: `propel`
+
+role :web,        domain                         # Your HTTP server, Apache/etc
+role :app,		  domain
+role :db,         domain, :primary => true       # This may be the same as your `Web` server
+
+set :shared_files,      ["app/config/parameters.yml"]
+set :shared_children,     [app_path + "/logs", web_path + "/uploads", "vendor"]
+
+set :use_composer, true
+set :update_vendors, true
+
+task :upload_parameters do
+  origin_file = "app/config/parameters.yml"
+  destination_file = shared_path + "/app/config/parameters.yml" # Notice the
+  shared_path
+
+  try_sudo "mkdir -p #{File.dirname(destination_file)}"
+  top.upload(origin_file, destination_file)
+end
+
+after "deploy:setup", "upload_parameters"
+
+set  :keep_releases,  3
+
+# Be more verbose by uncommenting the following line
+# logger.level = Logger::MAX_LEVEL
